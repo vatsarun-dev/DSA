049bd2c8157dd4e08788967ba061cc44eee58363
Arun Vats <vatsarun58@gmail.com>
2026-09-14 17:24:34 +0530

    Time: 0 ms (100%), Space: 55.5 MB (45.6%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.ts
@@ -0,0 +1,10 @@
+function removeElement(nums: number[], val: number): number {
+ let k:number=0
+ for(let i:number =0;i<nums.length;i++){
+    if(nums[i]!==val){
+        nums[k]=nums[i]
+        k++
+    }
+ }
+ return k
+};
