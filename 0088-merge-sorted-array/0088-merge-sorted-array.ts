91f98185c1f5b26516572a8b870a501fb591bc2b
Arun Vats <vatsarun58@gmail.com>
2026-09-15 17:20:43 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.ts
@@ -0,0 +1,19 @@
+/**
+ Do not return anything, modify nums1 in-place instead.
+ */
+function merge(a: number[], m: number, b: number[], n: number): void {
+   let index:number =m+n-1
+   let i:number=m-1
+   let j:number=n-1
+   while(i>=0 && j>=0){
+    if(a[i]<b[j]){
+        a[index--]=b[j--]
+    }
+    else{
+        a[index--]=a[i--]
+    }
+   }
+   while(j<=0){
+    a[index--]=b[j--]
+   }
+};
