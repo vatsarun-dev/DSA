679f5dd8c3d7a562b9dfaacfd410706959ec85b5
Arun Vats <vatsarun58@gmail.com>
2026-09-15 18:47:01 +0530

    Time: 0 ms (100%), Space: 54.9 MB (67.78%) - LeetHub

---
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.ts
@@ -0,0 +1,6 @@
+function check(a: number[]): boolean {
+    let drop:number=0
+    for(let i:number=0;i<a.length;i++){
+    if(a[i]>a[(i + 1) % a.length]) drop++
+}
+return (drop === 1 || drop === 0) ? true : false;};
