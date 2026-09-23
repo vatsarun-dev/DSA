2cad43ffca833416b492f94304500f2f18c68f6e
Arun Vats <vatsarun58@gmail.com>
2026-09-23 20:58:43 +0530

    Time: 0 ms (100%), Space: 56.6 MB (18.94%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.ts
+++ 0088-merge-sorted-array/0088-merge-sorted-array.ts
@@ -2,18 +2,10 @@
  Do not return anything, modify nums1 in-place instead.
  */
 function merge(a: number[], m: number, b: number[], n: number): void {
-   let index:number =m+n-1
-   let i:number=m-1
-   let j:number=n-1
-   while(i>=0 && j>=0){
-    if(a[i]<b[j]){
-        a[index--]=b[j--]
+    let i:number=m-1, j:number=n-1, index:number=m+n-1
+    while(i>=0 && j>=0){
+        if(a[i]>=b[j]) a[index--]=a[i--]
+        else a[index--]=b[j--]
     }
-    else{
-        a[index--]=a[i--]
-    }
-   }
-   while(j>=0){
-    a[index--]=b[j--]
-   }
+    while(j>=0) a[index--]=b[j--]
 };
