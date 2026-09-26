ca1f12205c957fb2011e3b820a036356eb37895f
Arun Vats <vatsarun58@gmail.com>
2026-09-26 18:39:12 +0530

    Time: 0 ms (100%), Space: 53.6 MB (100%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.ts
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.ts
@@ -1,9 +1,8 @@
 function diagonalSum(a: number[][]): number {
     let sum:number=0
     for(let i=0;i<a.length;i++){
-        for(let j=0;j<a[i].length;j++){
-            if(i==j || i+j == a[i].length-1) sum+=a[i][j]
-        }
+        sum+=a[i][i]
+        if(i!= a.length-1-i) sum+=a[i][a.length-1-i]
     }
     return sum
 };
